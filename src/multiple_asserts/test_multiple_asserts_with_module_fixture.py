a86a88b65b4ed269ab30642a7c961be6d8473915
Hai Vu <haivu2004@gmail.com>
2025-02-12 10:55:25 -0800

    Update multiple asserts

--- src/multiple_asserts/test_multiple_asserts_with_module_fixture.py
+++ src/multiple_asserts/test_multiple_asserts_with_module_fixture.py
@@ -3,30 +3,71 @@ Use a fixture to perform the prepare- and action phrase. Then each test
 will assert just once.
 """
 
+import http
+
 import pytest
+import requests
+
+# ======================================================================
 
 
 @pytest.fixture(scope="module")
-def actual():
-    """Perform the action and return the actual data."""
-    return {
-        "name": "env1",
-        "description": "My Environment",
-        "tags": ["sandbox", "experimental"],
-    }
+def response():
+    """Perform the action and return the response."""
+    return requests.get("https://jsonplaceholder.typicode.com/posts/1")
 
 
-def test_name(actual):
-    assert actual["name"] == "env1"
+@pytest.fixture(scope="module")
+def payload(response):
+    """
+    Return the payload part
+
+    Sample payload:
+
+        {
+            "userId": 1,
+            "id": 1,
+            "title": "sunt aut ...",
+            "body": "quia et ..."
+        }
+    """
+    return response.json()
 
 
-def test_tag_sandbox(actual):
-    assert "sandbox" in actual["tags"]
+# ======================================================================
 
 
-def test_tag_experimental(actual):
-    assert "experimental" in actual["tags"]
+def test_status_code(response):
+    assert response.status_code == http.HTTPStatus.OK
 
 
-def test_description(actual):
-    assert actual["description"] == "My Environment"
+def test_ok(response):
+    assert response.ok
+
+
+def test_userid(payload):
+    assert payload["userId"] == 1
+
+
+def test_id(payload):
+    assert payload["id"] == 1
+
+
+def test_title(payload):
+    assert payload["title"]
+
+
+def test_body(payload):
+    assert payload["body"]
+
+
+def test_content_type(response: requests.Response):
+    assert response.headers["Content-Type"] == "application/json; charset=utf-8"
+
+
+def test_redirect(response: requests.Response):
+    assert response.is_redirect is False
+
+
+def test_no_cookie(response: requests.Response):
+    assert len(response.cookies) == 0
