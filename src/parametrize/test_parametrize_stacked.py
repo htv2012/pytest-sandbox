eed495ad2618ba5c95dba2fb41a700b306a6d8b2
Hai Vu <haivu2004@gmail.com>
2025-02-12 23:24:32 -0800

    Update stacked

--- src/parametrize/test_parametrize_stacked.py
+++ src/parametrize/test_parametrize_stacked.py
@@ -6,3 +6,11 @@ import pytest
 @pytest.mark.parametrize("first", [1, 2, 3])
 def test_add(first, second):
     pass
+
+
+@pytest.mark.parametrize(
+    "port", [pytest.param("80", id="http"), pytest.param("443", id="https")]
+)
+@pytest.mark.parametrize("method", "get put post delete".split())
+def test_add_using_id(method, port):
+    pass
