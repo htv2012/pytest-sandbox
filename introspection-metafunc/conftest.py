75fec156f579f584afe44df7407c6727110c413c
Hai Vu <haivu2004@gmail.com>
2025-03-14 10:17:19 -0700

    Update conftest.py in introspection-metafunc

--- introspection-metafunc/conftest.py
+++ introspection-metafunc/conftest.py
@@ -1,14 +1,6 @@
 import pytest
+from pytest_sandbox import introspect
 
 
 def pytest_generate_tests(metafunc: pytest.Metafunc):
-    print("\n\n# Explore Metafunc")
-    for name in dir(metafunc):
-        if name.startswith("_"):
-            continue
-        obj = getattr(metafunc, name)
-        if callable(obj):
-            print(f"  - {name}()")
-        else:
-            print(f"  - {name}: {obj.__class__.__name__}")
-    print()
+    introspect.explore(metafunc, "Metafunc")
