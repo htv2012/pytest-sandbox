730b0a0879dad3b1f82d300724f95fe45b752fc4
Hai Vu <haivu2004@gmail.com>
2025-03-16 08:06:52 -0700

    Add test-data

---
+++ test-data/test_data.py
@@ -0,0 +1,19 @@
+"""
+The `request` fixture has an attribute `path` which is the path of the
+test script. We can use that in order to locate the `data` dir
+"""
+
+import pytest
+import yaml
+
+
+@pytest.fixture
+def data_path(request):
+    script_dir = request.path.parent
+    return script_dir / "data"
+
+
+def test_data(data_path):
+    with open(data_path / "data.yaml") as stream:
+        data = yaml.safe_load(stream)
+    assert data == {"uid": 501, "shell": "zsh"}
