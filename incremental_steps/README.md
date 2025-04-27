ad48d714a354ba441297d6e8b22e38d7206b062c
Hai Vu <haivu2004@gmail.com>
2025-04-26 20:27:59 -0700

    Add incremental_steps

---
+++ incremental_steps/README.md
@@ -0,0 +1,3 @@
+This tests demonstrate the idea that sometimes when tests are done in
+incremental steps. If a step is failed, all subsequent steps do not make
+sense, thus are failed by default.
---
+++ incremental_steps/test_steps.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+@pytest.mark.incremental
+class TestUser:
+    def test_login(self):
+        pass
+
+    def test_modify(self):
+        assert False
+
+    def test_act(self):
+        pass
