954a27e244d79c6b971b16353fc71f94c4e61062
Hai Vu <haivu2004@gmail.com>
2025-07-17 22:58:22 -0700

    Add multiple asserts using exceptions

---
+++ multiple_asserts/test_using_multiple_exceptions.py
@@ -0,0 +1,24 @@
+import pytest
+
+
+def _verify_even(n):
+    assert n % 2 == 0, "Even test"
+
+
+def _verify_positive(n):
+    assert n > 0, "Positive test"
+
+
+def test_value():
+    value = -5
+    errors = []
+    for check in [_verify_even, _verify_positive]:
+        try:
+            check(value)
+        except AssertionError as error:
+            errors.append(error)
+
+    if errors:
+        msg = "\n".join(str(e) for e in errors)
+        msg = f"\n{msg}"
+        pytest.fail(msg)
