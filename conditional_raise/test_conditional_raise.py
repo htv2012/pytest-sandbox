d66ece0e9cc8e6f9c42e4ed9f1d09486d6313f75
Hai Vu <haivu2004@gmail.com>
2025-10-11 22:03:12 -0700

    update conditional_raise

--- conditional_raise/test_conditional_raise.py
+++ conditional_raise/test_conditional_raise.py
@@ -2,26 +2,24 @@
 Some tests are expected to raise and while others don't
 """
 
-import contextlib
+from contextlib import nullcontext
 
 import pytest
 
 
-def expected_error(klass):
-    if klass is None:
-        return contextlib.nullcontext()
-    return pytest.raises(klass)
-
-
 @pytest.mark.parametrize(
-    ["x", "y", "expected", "error"],
+    "x, y, expected, context",
     [
-        pytest.param(5, 2, 2.5, None, id="happy path"),
-        pytest.param(5, 2, 2, None, id="expected fail", marks=pytest.mark.xfail),
-        pytest.param(5, 0, None, ZeroDivisionError, id="div by zero"),
-        pytest.param(5, "a", None, TypeError, id="int div by str"),
+        pytest.param(5, 2, 2, nullcontext(), id="happy path"),
+        pytest.param(
+            5, 2, 3, nullcontext(), marks=pytest.mark.xfail, id="expected fail"
+        ),
+        pytest.param(
+            5, 0, nullcontext(), pytest.raises(ZeroDivisionError), id="div by zero"
+        ),
+        pytest.param(5, "a", None, pytest.raises(TypeError), id="int div by str"),
     ],
 )
-def test_division(x, y, expected, error):
-    with expected_error(error):
-        assert x / y == expected
+def test_division(x, y, expected, context):
+    with context:
+        assert x // y == expected
