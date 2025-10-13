fa55e689946781411e44d9b7b9127d532ebe2328
Hai Vu <haivu2004@gmail.com>
2025-10-13 09:05:56 -0700

    refactor: Compact code

--- parametrize-cmdline/conftest.py
+++ parametrize-cmdline/conftest.py
@@ -3,9 +3,6 @@ import os
 
 import pytest
 
-TEST_SERVERS = "default1.com,default2.com"
-TEST_PORTS = "8001,8002"
-
 
 def pytest_addoption(parser: pytest.Parser):
     parser.addoption("--test-servers", help="Servers list")
@@ -14,23 +11,19 @@ def pytest_addoption(parser: pytest.Parser):
 
 def _get_test_data(config, name, default):
     value = config.getoption(name)
-    if value is None:
-        value = os.getenv(name.upper())
-    if value is None:
-        value = default
+    value = value or os.getenv(name.upper(), default)
     return value.split(",")
 
 
 def pytest_generate_tests(metafunc: pytest.Metafunc):
-    global DEFAULT_SERVERS
+    TEST_SERVERS = "default1.com,default2.com"
+    TEST_PORTS = "8001,8002"
 
     if "server" in metafunc.fixturenames:
-        # breakpoint()
         servers = _get_test_data(metafunc.config, "test_servers", TEST_SERVERS)
         metafunc.parametrize("server", servers, scope="session")
 
     if "port" in metafunc.fixturenames:
-        # breakpoint()
         ports = _get_test_data(metafunc.config, "test_ports", TEST_PORTS)
         ports = [int(port) for port in ports]
         metafunc.parametrize("port", ports, scope="session")
