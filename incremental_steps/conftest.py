66ec07455e81b678d54342ed6962e9a786944c9d
Hai Vu <haivu2004@gmail.com>
2025-10-25 23:02:36 -0700

    refactor: rename vars

--- incremental_steps/conftest.py
+++ incremental_steps/conftest.py
@@ -1,12 +1,10 @@
 # content of conftest.py
 
-from collections import defaultdict
 
 import pytest
 
 # store history of failures per test class name and per index in parametrize (if parametrize used)
-# _test_failed_incremental: Dict[str, Dict[Tuple[int, ...], str]] = {}
-_test_failed_incremental = defaultdict(lambda: defaultdict(dict))
+_failed = {}
 
 
 def pytest_runtest_makereport(item, call):
@@ -24,8 +22,9 @@ def pytest_runtest_makereport(item, call):
 
     # retrieve the name of the test function
     test_name = item.originalname or item.name
-    # store in _test_failed_incremental the original name of the failed test
-    _test_failed_incremental[cls_name][parametrize_index] = test_name
+    # store in _failed the original name of the failed test
+    # _failed[cls_name][parametrize_index] = test_name
+    _failed.setdefault(cls_name, {}).setdefault(parametrize_index, test_name)
 
 
 def pytest_runtest_setup(item):
@@ -35,7 +34,7 @@ def pytest_runtest_setup(item):
     # retrieve the class name of the test
     cls_name = str(item.cls)
     # check if a previous test has failed for this class
-    if cls_name not in _test_failed_incremental:
+    if cls_name not in _failed:
         return
 
     # retrieve the index of the test (if parametrize is used in combination with incremental)
@@ -43,7 +42,8 @@ def pytest_runtest_setup(item):
         tuple(item.callspec.indices.values()) if hasattr(item, "callspec") else ()
     )
     # retrieve the name of the first test function to fail for this class name and index
-    test_name = _test_failed_incremental[cls_name].get(parametrize_index, None)
+    # test_name = _failed[cls_name].get(parametrize_index, None)
+    test_name = _failed.get(cls_name, {}).get(parametrize_index, None)
     # if name found, test has failed for the combination of class name & test name
     if test_name is not None:
         pytest.skip("previous test failed: {}".format(test_name))
