6082e3b300e92d15234b6789251e2b8275fa4f80
Hai Vu <haivu2004@gmail.com>
2025-12-21 00:22:12 -0800

    refactor: Remove create_param

--- parametrize/from_file/test/pytestlib.py
+++ parametrize/from_file/test/pytestlib.py
@@ -4,19 +4,16 @@ import pytest
 import yaml
 
 
-def create_param(argnames, test_id, param):
-    args = [param[name] for name in argnames]
-    return pytest.param(*args, id=test_id)
-
-
 def parametrize_from_file(path: pathlib.Path, test_name: str, argnames: list[str]):
     path = pathlib.Path(path)
     with open(path, "rb") as stream:
         content = yaml.safe_load(stream)
 
     test_params = content[test_name]
-    argvalues = [
-        create_param(argnames, test_id, params)
-        for test_id, params in test_params.items()
-    ]
-    return pytest.mark.parametrize(argnames, argvalues)
+    return pytest.mark.parametrize(
+        argnames,
+        [
+            pytest.param(*[params[name] for name in argnames], id=test_id)
+            for test_id, params in test_params.items()
+        ],
+    )
