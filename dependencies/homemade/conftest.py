bf58eddea2716f98dbf173c5fc0a352f87a3ffc4
Hai Vu <haivu2004@gmail.com>
2026-03-31 21:54:31 -0700

    fix: KeyError in conftest.py

--- dependencies/homemade/conftest.py
+++ dependencies/homemade/conftest.py
@@ -3,6 +3,7 @@ import graphlib
 
 import pytest
 
+# {nodeid: passed/failed/skipped/error}
 OUTCOME = {}
 
 
@@ -16,17 +17,21 @@ def get_dependencies(item: pytest.Function) -> set[str]:
 def pytest_collection_modifyitems(
     session: pytest.Session, config: pytest.Config, items: list[pytest.Function]
 ):
+    """Order the tests based on dependencies resolution."""
     dependency = collections.defaultdict(set)  # {test: set of tests}
     for it in items:
         dependency[it.function.__name__].update(get_dependencies(it))
 
+    # Resolve dependencies and establish the tests order
     sorter = graphlib.TopologicalSorter(dependency)
     test_order = list(sorter.static_order())
 
+    # Order the tests
     items.sort(key=lambda it: test_order.index(it.function.__name__))
 
 
 def pytest_runtest_setup(item: pytest.Function):
+    """Skip a test if at least one dependencies not passed."""
     mark = next(item.iter_markers(name="depends_on"), None)
     if mark is None or not mark.args:
         return
@@ -41,6 +46,7 @@ def pytest_runtest_setup(item: pytest.Function):
 
 def pytest_runtest_logreport(report):
     """Called for each phase of each test (setup/call/teardown)."""
-    if report.when != "call":  # only store final outcome of the test
-        return
-    OUTCOME[report.nodeid] = report.outcome  # 'passed', 'failed', or 'skipped'
+    # Normally, outcome is recorded at "call" time
+    # When a test is skipped, outcome at setup is "skipped"
+    if report.when == "setup" or report.when == "call":
+        OUTCOME[report.nodeid] = report.outcome  # 'passed', 'failed', or 'skipped'
