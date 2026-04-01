f7acc5a9a07e4b4fe2172b9936fd469c5211f917
Hai Vu <haivu2004@gmail.com>
2026-03-31 22:34:43 -0700

    fix: missing jupyter

---
+++ jupyter_notebook/jupyterlib.py
@@ -0,0 +1,21 @@
+import subprocess
+import tempfile
+
+
+def is_valid_notebook(notebook_path: str):
+    with tempfile.TemporaryDirectory() as tmp_path:
+        process = subprocess.run(
+            [
+                "jupyter",
+                "nbconvert",
+                "--to",
+                "markdown",
+                "--execute",
+                "--output-dir",
+                tmp_path,
+                str(notebook_path),
+            ],
+            check=False,
+            capture_output=True,
+        )
+    return process.returncode == 0
--- jupyter_notebook/test_notebooks.py
+++ jupyter_notebook/test_notebooks.py
@@ -1,36 +1,16 @@
 import pathlib
-import subprocess
 
+import jupyterlib
 import pytest
-
-
-@pytest.fixture(scope="module")
-def notebooks_dir():
-    return pathlib.Path(__file__).parent / "notebooks"
 
 
 @pytest.mark.parametrize(
     ["filename", "expected"],
     [
-        pytest.param("good.ipynb", 0, id="good"),
-        pytest.param("bad.ipynb", 1, id="bad"),
+        pytest.param("good.ipynb", True, id="good"),
+        pytest.param("bad.ipynb", False, id="bad"),
     ],
 )
-def test_notebook(filename, expected, notebooks_dir, tmp_path):
-    notebook_path = notebooks_dir / filename
-    process = subprocess.run(
-        [
-            "jupyter",
-            "nbconvert",
-            "--to",
-            "markdown",
-            "--execute",
-            "--output-dir",
-            str(tmp_path),
-            str(notebook_path),
-        ],
-        check=False,
-        capture_output=True,
-    )
-
-    assert process.returncode == expected
+def test_notebook(filename, expected, tmp_path):
+    notebook_path = str(pathlib.Path(__file__).parent / "notebooks" / filename)
+    assert jupyterlib.is_valid_notebook(notebook_path) is expected
